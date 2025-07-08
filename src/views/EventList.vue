a91a6d1d7a1d731b465f503954330378183ad09f
rajlanjewar01 <rajl.perficient@gmail.com>
2025-07-08 13:10:32 +0530

    fetching state with ACTION

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,39 +1,36 @@
 <template>
-  <h1>Events for Good</h1>
-  <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
-  </div>
+	<h1>Events for Good</h1>
+	<div class="events">
+		<EventCard v-for="event in events" :key="event.id" :event="event" />
+	</div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+
 export default {
-  name: 'EventList',
-  components: {
-    EventCard
-  },
-  data() {
-    return {
-      events: null
-    }
-  },
-  created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
+	name: 'EventList',
+
+	components: {
+		EventCard
+	},
+
+	computed: {
+		events() {
+			return this.$store.state.events
+		}
+	},
+
+	created() {
+		this.$store.dispatch('fetchEvents');
+	}
 }
 </script>
 
 <style scoped>
 .events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 }
 </style>
